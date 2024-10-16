3e02a23d0399b18bea2a066a4229e003f8ef8541
Rafael Oliveira <147670578+oliveirarafaa@users.noreply.github.com>
2024-10-15 21:12:11 -0300

    Atualização

---
+++ front-projeto/src/pages/cadastro/index.jsx
@@ -0,0 +1,20 @@
+import './Style.css'
+
+function Castro() {
+
+  return (
+      <div className='container'>
+          <form>
+            <h1>Login</h1>
+            <input name='email' type="email" placeholder='E-mail' autocomplete="email" required maxlength="30"/>
+            <input name='senha' type='password' placeholder='Senha' autocomplete="current-password" required minlength="8" maxlength="20"/>
+            <button type='submit'>Entrar</button>
+            <a href="#" >Esqueci a senha</a>
+            <a href="#" >Cadastrar</a>
+          </form>
+          
+      </div>
+  )
+}
+
+export default Home
